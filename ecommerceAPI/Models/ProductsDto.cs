657cc6790222647e7540d9474663d6c812bafc19
Kacper <kacper070302@gmail.com>
2025-03-29 18:59:33 +0100

    DTO change

--- ecommerceAPI/Models/ProductsDto.cs
+++ ecommerceAPI/Models/ProductsDto.cs
@@ -19,6 +19,8 @@ namespace ecommerceAPI.Models
         public double Price { get; set; }
         public bool IsAvailable { get; set; }
 
+        public DateTime CreatedDate { get; set; }
+
 
     }
 }
